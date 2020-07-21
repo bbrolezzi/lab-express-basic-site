112720286ebab7c4b454f850193408be3d0309d0
bbrolezzi <bbrolezzi@users.noreply.github.com>
2020-07-21 18:18:21 +0100

    Don't know why the navbar is showing up twice

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const express = require('express');
+//hbs activation
+const hbs = require('hbs');
 const app = express();
 app.use(express.static('public'));
+//hbs template set
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.get('/home', (request, response) => {
-  response.sendFile(__dirname + '/views/home.html');
-  console.log('test');
+  //response.sendFile(__dirname + '/views/home.html');
   //response.send('Test Home');
+  response.render('layout');
 });
 app.get('/about', (request, response) => {
-  response.sendFile(__dirname + '/views/about.html');
-  ///response.send('Test About');
+  //response.sendFile(__dirname + '/views/about.html');
+  //response.send('Test About');
+  response.render('layout');
 });
 app.get('/works', (request, response) => {
-  response.sendFile(__dirname + '/views/works.html');
-  response.send('Test Works');
+  //response.sendFile(__dirname + '/views/works.html');
+  //response.send('Test Works');
+  response.render('layout');
 });
 app.get('/photogallery', (request, response) => {
-  response.sendFile(__dirname + '/views/photogallery.html');
-  response.send('Test Photogallery');
+  //response.sendFile(__dirname + '/views/photogallery.html');
+  //response.send('Test Photogallery');
+  response.render('layout');
 });
-app.listen(3000);
+app.listen(3001);
